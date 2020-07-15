1bca2c4eeb8c702fbe25a8bb1d85ea752a25387e
賓 <bing5300@gmail.com>
2020-07-15 23:57:01 +0800

    first commit

---
+++ TcpClient.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[CLSCompliant(false)]
+public class TcpClient : MonoBehaviour {
+
+    Socket clientSocket;
+    IPEndPoint ipEnd;
+    byte[] dataBuffer = new byte[4096];
+    Thread connectThread;
+
+    private int port = 5566;
+
+    //init
+    public void InitSocket(string ipAddr) {
+        IPAddress ip = IPAddress.Parse(ipAddr);
+        ipEnd = new IPEndPoint(ip, port);
+        //create socket instance
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.BeginConnect(ipEnd, new AsyncCallback(connectCallback), null);
+    }
+
+    private void connectCallback(IAsyncResult async) {
+        try {
+            clientSocket.EndConnect(async);
+            waitData(clientSocket);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private AsyncCallback socketCallBack;
+    private void waitData(Socket socket) {
+        try {
+            if (socketCallBack == null)
+                socketCallBack = new AsyncCallback(onDataReceive);
+
+            socket.BeginReceive(dataBuffer, 0, dataBuffer.Length, SocketFlags.None, socketCallBack, socket);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private void onDataReceive(IAsyncResult async) {
+        try {
+            Socket client = (Socket)async.AsyncState;
+            client.EndReceive(async);
+            dataHandle(dataBuffer);
+            //complete get data, wait next data
+            waitData(clientSocket);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    //////////   custom area /////////////////
+
+    //send data to server
+    public void SocketSend(byte[] sendMsg) {
+        clientSocket.BeginSend(sendMsg, 0, sendMsg.Length, 0, new AsyncCallback(sendCallback), null);
+    }
+
+    private void sendCallback(IAsyncResult async) {
+        try {
+            int bytesSent = clientSocket.EndSend(async);
+            Debug.Log("Sent" + bytesSent.ToString() +  "bytes to server.");
+        }
+        catch(Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    //all data get from server would be handled here
+    private static void dataHandle(byte[] data) {
+        //message from server
+        NetMgr.OnMsgRcv(data, true);
+    }
+
+    //handle exception
+    void socketErrHandle(SocketException e) {
+        Debug.Log("error" + e);
+        MainMgr.inst.panelWaitingList.Enqueue("NetErrorPanel");
+        //UIMgr.inst.generatePanel("NetErrorPanel");
+    }
+
+
+    //public UnityEngine.UI.Text txt = null;
+    //public UnityEngine.UI.InputField input = null;
+    //string str = "";
+
+    //public static byte[] Trans2byte<T>(T data) {
+    //    byte[] dataBytes;
+    //    using (MemoryStream ms = new MemoryStream()) {
+    //        BinaryFormatter bf1 = new BinaryFormatter();
+    //        bf1.Serialize(ms, data);
+    //        dataBytes = ms.ToArray();
+    //    }
+    //    return dataBytes;
+    //}
+
+    //void Update() {
+    //    txt.text = str;
+    //}
+
+    //void Start() {
+    //    InitSocket("ip");
+
+    //}
+    //trigger by button
+    //public void TESTSEND() {
+    //    Debug.Log("send" + input.text);
+    //    SocketSend(Trans2byte(input.text));
+    //}
+
+    //////////   custom area /////////////////
+
+    private void SocketQuit() {
+        if (clientSocket != null)
+            clientSocket.Close();
+        print("diconnect client");
+    }
+
+    void Awake() {
+        //set persist node
+        DontDestroyOnLoad(this);
+    }
+
+    //程式退出則關閉連線
+    void OnApplicationQuit() {
+        SocketQuit();
+    }
+}
---
+++ TcpServer.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+[CLSCompliant(false)]
+public class TcpServer : MonoBehaviour {
+    Socket serverSocket;
+
+    public class SocketPack {
+        public Socket currentSocket;
+        public byte[] dataBuffer = new byte[4096];
+    }
+
+    private int port = 5566;
+    private const int MAX_CLIENTS = 20;
+    private Socket[] socketChannel = new Socket[MAX_CLIENTS];
+
+    //init
+    void InitSocket() {
+        //listen any IP
+        IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, port);
+        //create socket instance
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverSocket.Bind(ipEnd);
+
+        //開始偵聽 "同時要求連線最大值" 3
+        serverSocket.Listen(3);
+        serverSocket.BeginAccept(new AsyncCallback(OnClientConnect), null);
+    }
+
+    public class NoSocketAvailableException : Exception {
+        new public string Message = "NoSocketAvailableException";
+    }
+
+    // find avaliable channel
+    private int FindEmptyChannel() {
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (socketChannel[i] == null || !socketChannel[i].Connected)
+                return i;
+        }
+        return -1;
+    }
+
+    private void OnClientConnect(IAsyncResult async) {
+        try {
+            int emptyChannelIndex = -1;
+            if (serverSocket == null)
+                return;
+
+            Socket tmpSocket = serverSocket.EndAccept(async);
+            EndPoint remoteEndPoint = tmpSocket.RemoteEndPoint;
+            //get avaliable channel
+            emptyChannelIndex = FindEmptyChannel();
+            if (emptyChannelIndex == -1)
+                throw new NoSocketAvailableException();
+            //handle tmp socket
+            socketChannel[emptyChannelIndex] = tmpSocket;
+            tmpSocket = null;
+
+            waitData(socketChannel[emptyChannelIndex]);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+        finally {
+            //release lock
+            serverSocket.BeginAccept(new AsyncCallback(OnClientConnect), null);
+        }
+    }
+
+    private static AsyncCallback socketCallBack;
+    private static void waitData(Socket socket) {
+        try {
+            if (socketCallBack == null)
+                socketCallBack = new AsyncCallback(onDataReceive);
+
+            SocketPack socketPack = new SocketPack();
+            socketPack.currentSocket = socket;
+            socket.BeginReceive(socketPack.dataBuffer, 0, socketPack.dataBuffer.Length, SocketFlags.None, socketCallBack, socketPack);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private static void onDataReceive(IAsyncResult async) {
+        try {
+            SocketPack socketData = (SocketPack)async.AsyncState;
+            socketData.currentSocket.EndReceive(async);
+            dataHandle(socketData.dataBuffer);
+            //complete get data, wait next
+            waitData(socketData.currentSocket);
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    //////////   custom area /////////////////
+
+    //all data get from client would be handled here
+    private static void dataHandle(byte[] data) {
+        //message from client
+        NetMgr.OnMsgRcv(data, false);
+    }
+
+    //send data to all client
+    public void SocketSend(byte[] sendMsg) {
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (socketChannel[i] != null && socketChannel[i].Connected) {
+                socketChannel[i].Send(sendMsg, sendMsg.Length, SocketFlags.None);
+            }
+        }
+    }
+
+    //////////   custom area /////////////////
+    
+    //連線關閉
+    private void SocketQuit() {
+        //close client
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (socketChannel[i] != null)
+                socketChannel[i].Close();
+        }
+        //close server
+        if (serverSocket != null)
+            serverSocket.Close();
+        print("diconnect server");
+    }
+
+    void Awake() {
+        //set persist node
+        DontDestroyOnLoad(this);
+    }
+
+    void Start() {
+        //init server
+        InitSocket();
+    }
+
+    void OnApplicationQuit() {
+        SocketQuit();
+    }
+}
